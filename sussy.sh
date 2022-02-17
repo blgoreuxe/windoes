ad23505f9662d466666553d8d9d3611de94f773f
blgoreuxe <90001868+blgoreuxe@users.noreply.github.com>
2022-02-17 17:06:25 -0500

    Update sussy.sh

--- sussy.sh
+++ sussy.sh
@@ -1,12 +1,12 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
+nohup ./ngrok tcp 5900
 echo Please wait for installing...
-apt update -y > /dev/null 2>&1
+apt update -y
 echo "Installing QEMU (2-3m)..."
-apt install qemu-system-x86 curl -y > /dev/null 2>&1
+apt install qemu-system-x86 curl -y
 echo Downloading Windows Disk...
 curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.3/providers/qemu.box
 echo "Windows 7 x86 Lite On Google Colab"
@@ -15,4 +15,4 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by sussy baka
-qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 1000M -machine usb=on -device usb-tablet > /dev/null 2>&1
+qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 1000M -machine usb=on -device usb-tablet
